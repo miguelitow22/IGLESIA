5cbbc86c7ddefc2b21419b705488a0d81b294333
miguelitow22 <miguelopsal@gmail.com>
2024-10-24 14:40:48 -0500

    local

--- backend/routes/ministerios.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMinisterios } = require('../controllers/ministerioController');
-
-router.get('/', getMinisterios);
-
-module.exports = router;
-  
--- backend/routes/requests.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createRequest } = require('../controllers/requestsController');
-
-router.post('/', createRequest);
-
-module.exports = router;
